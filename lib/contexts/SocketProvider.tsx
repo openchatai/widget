0680cf3f36763b1e8ee33488569d0e83bb615392
ah7255703 <ah7255703@gmail.com>
2024-06-19 17:26:47 +0300

    chore: Refactor SocketProvider to use useState for socket initialization

--- lib/contexts/SocketProvider.tsx
+++ lib/contexts/SocketProvider.tsx
@@ -1,5 +1,5 @@
 import { Socket, io } from "socket.io-client";
-import { ReactNode, useCallback, useEffect, useMemo, useReducer } from "react";
+import { ReactNode, useCallback, useEffect, useReducer, useState } from "react";
 import { useConfigData } from "./ConfigData";
 import { createSafeContext } from "./createSafeContext";
 import { useWidgetState } from "./WidgetState";
@@ -18,16 +18,16 @@ type SocketContextData = {
 
 type ActionType =
   | {
-      type: "RECONNECT_ATTEMPT";
-      payload: number;
-    }
+    type: "RECONNECT_ATTEMPT";
+    payload: number;
+  }
   | {
-      type: "CONNECTED";
-    }
+    type: "CONNECTED";
+  }
   | {
-      type: "DISCONNECTED";
-      payload: string;
-    };
+    type: "DISCONNECTED";
+    payload: string;
+  };
 
 function socketReducer(state: SocketState, action: ActionType) {
   return produce(state, (draft) => {
@@ -55,18 +55,19 @@ function SocketProvider({ children }: { children: ReactNode }) {
     reason: null,
     reconnectAttempts: null,
   });
-
+  const [socket, setSocket] = useState<Socket | null>(null);
+  useEffect(() => {
+    if (socket) return;
+    if (socketUrl) {
+      setSocket(io(socketUrl, {
+        autoConnect: false,
+        transports: ["websocket"],
+      }));
+    }
+  }, []);
   const { socketUrl } = useConfigData();
   const [open] = useWidgetState();
 
-  const socket = useMemo(
-    () =>
-      io(socketUrl, {
-        autoConnect: false,
-        transports: ["websocket"],
-      }),
-    [socketUrl]
-  );
 
   const handleConnect = useCallback(() => {
     dispatch({ type: "CONNECTED" });
@@ -82,31 +83,31 @@ function SocketProvider({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     if (open) {
-      socket.connect();
+      socket?.connect();
     } else {
-      socket.disconnect();
+      socket?.disconnect();
     }
     return () => {
-      socket.disconnect();
+      socket?.disconnect();
     };
   }, [open, socket]);
 
   useEffect(() => {
     // Fired upon a successful connection.
-    socket.on("connect", handleConnect);
+    socket?.on("connect", handleConnect);
     // Fired upon a disconnection.
-    socket.on("disconnect", handleDisconnect);
+    socket?.on("disconnect", handleDisconnect);
     // Fired upon an attempt to reconnect.
-    socket.on("reconnect_attempt", handleReconnectAttempt);
+    socket?.on("reconnect_attempt", handleReconnectAttempt);
     return () => {
-      socket.off("connect", handleConnect);
-      socket.off("disconnect", handleDisconnect);
-      socket.off("reconnect_attempt", handleReconnectAttempt);
+      socket?.off("connect", handleConnect);
+      socket?.off("disconnect", handleDisconnect);
+      socket?.off("reconnect_attempt", handleReconnectAttempt);
     };
   }, [socket, handleConnect, handleDisconnect, handleReconnectAttempt]);
 
   return (
-    <SocketSafeProvider value={{ __socket: socket, state }}>
+    <SocketSafeProvider value={{ __socket: socket!, state }}>
       {children}
     </SocketSafeProvider>
   );
