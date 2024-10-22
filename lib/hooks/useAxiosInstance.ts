e8f4792cc8897ac18447781aae862bdf4778c49e
ah7255703 <ah7255703@gmail.com>
2024-10-22 17:08:49 +0300

    better fetching

--- lib/hooks/useAxiosInstance.ts
+++ lib/hooks/useAxiosInstance.ts
@@ -1,3 +1,5 @@
+import { ChatSessionType } from "@lib/types/schemas";
+import { PreludeData, WorkingHours } from "@lib/utils";
 import axios from "axios";
 import { useMemo } from "react";
 
@@ -5,28 +7,41 @@ type Options = {
   apiUrl: string;
   botToken: string;
 };
+const BotTokenHeader = "X-Bot-Token";
 
-function createAxiosInstance({
-  apiUrl,
-  botToken,
-}: {
-  apiUrl: string;
-  botToken: string;
-}) {
-  const instance = axios.create({
-    baseURL: apiUrl,
-    headers: {
-      "X-Bot-Token": botToken,
+export function useAxiosInstance(options: Options) {
+  const instance = useMemo(() => {
+    return axios.create({
+      baseURL: options.apiUrl,
+      headers: {
+        [BotTokenHeader]: options.botToken,
+      },
+    })
+  }, [options]);
+
+  const apis = useMemo(() => ({
+    createSession: (botToken: string) => {
+      return instance.post<ChatSessionType>("/chat-session/" + botToken);
     },
-  });
-  return instance;
-}
 
-export function useAxiosInstance({ apiUrl, botToken }: Options) {
-  return useMemo(() => {
-    return createAxiosInstance({
-      botToken: botToken,
-      apiUrl: apiUrl,
-    });
-  }, [botToken, apiUrl]);
+    /**
+  * get session data by id
+  * @param sessionId 
+  */
+    fetchSession: (sessionId: string) => {
+      return instance.get<ChatSessionType>(`/chat-session/one/${sessionId}`)
+    },
+
+    fetchPreludeData: () => {
+      return instance.get<PreludeData | undefined>("/widget/prelude");
+    },
+    /**
+      * get the organization office working hours.
+    */
+    getOfficeHours: () => {
+      return instance.get<WorkingHours>("/copilot/office-hours/public");
+    },
+  }), [instance])
+
+  return { apis, options }
 }
