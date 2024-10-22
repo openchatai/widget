34dffd5b6681fad23388914761b9b158c3997f3f
ah7255703 <ah7255703@gmail.com>
2024-10-22 23:30:24 +0300

    ui change

--- lib/@components/Loading.component.tsx
+++ lib/@components/Loading.component.tsx
@@ -2,14 +2,14 @@ import { Avatar, AvatarFallback } from "@lib/components/avatar";
 
 export function BotLoadingComponent() {
   return (
-    <div className="flex flex-col items-end wfull gap-1">
+    <div className="flex flex-col items-start w-full gap-1">
       <div className="flex items-center gap-1">
-        <Avatar className="animate-pulse bg-secondary">
+        <Avatar className="animate-pulse size-7 rounded-full bg-secondary">
           <AvatarFallback />
         </Avatar>
       </div>
-      <div className="w-1/2 min-w-[60%]">
-        <div className="bg-secondary p-6 rounded-lg animate-pulse" />
+      <div className="w-1/2 min-w-[80%]">
+        <div className="bg-secondary p-7 rounded-lg animate-pulse" />
       </div>
     </div>
   );
