c04fb3a3660b820abd93831c287176c905bc3aca
ah7255703 <ah7255703@gmail.com>
2024-10-22 23:31:40 +0300

    make the config provider more useful

--- lib/providers/ConfigDataProvider.tsx
+++ lib/providers/ConfigDataProvider.tsx
@@ -5,13 +5,18 @@ import { LocaleProvider } from "./LocalesProvider";
 import { useAxiosInstance } from "@lib/hooks";
 import { PreludeData } from "@lib/utils";
 import useSWR, { SWRResponse } from "swr";
+import { ComponentRegistry } from "./componentRegistry";
 
-type NonNullableSomeConfigKeys = MakeKeysNotNullable<WidgetOptions, "language" | "apiUrl" | "socketUrl">
+type NonNullableSomeConfigKeys = MakeKeysNotNullable<
+  WidgetOptions,
+  "language" | "apiUrl" | "socketUrl" | "headers" | "pathParams" | "queryParams"
+>;
 
 interface ConfigData extends NonNullableSomeConfigKeys {
   http: ReturnType<typeof useAxiosInstance>;
   botToken: string;
-  preludeSWR: SWRResponse<PreludeData | undefined, any>
+  preludeSWR: SWRResponse<PreludeData | undefined, any>;
+  componentStore: ComponentRegistry;
 }
 
 const [useConfigData, ConfigDataSafeProvider] = createSafeContext<ConfigData>();
@@ -25,6 +30,13 @@ export function ConfigDataProvider({
   data: WidgetOptions;
   children: ReactNode;
 }) {
+  const componentStore = useMemo(
+    () =>
+      new ComponentRegistry({
+        components: data.components,
+      }),
+    [data]
+  );
 
   const _data = useMemo(() => {
     return {
@@ -32,22 +44,28 @@ export function ConfigDataProvider({
       apiUrl: data.apiUrl ?? "https://api-v2.opencopilot.so/backend",
       socketUrl: data.socketUrl ?? "https://api-v2.opencopilot.so",
       language: data.language ?? DEFAULT_LANG,
-      botToken: data.token
+      botToken: data.token,
+      headers: data.headers ?? {},
+      pathParams: data.pathParams ?? {},
+      queryParams: data.queryParams ?? {},
     };
   }, [data]);
 
-  const http = useAxiosInstance({ apiUrl: _data.apiUrl, botToken: _data.token });
+  const http = useAxiosInstance({
+    apiUrl: _data.apiUrl,
+    botToken: _data.token,
+  });
 
   const preludeSWR = useSWR([http.options], async () => {
     const { data } = await http.apis.fetchPreludeData();
-    return data
+    return data;
   });
 
   return (
-    <ConfigDataSafeProvider value={{ ..._data, http, preludeSWR }}>
-      <LocaleProvider>
-        {children}
-      </LocaleProvider>
+    <ConfigDataSafeProvider
+      value={{ ..._data, http, preludeSWR, componentStore }}
+    >
+      <LocaleProvider>{children}</LocaleProvider>
     </ConfigDataSafeProvider>
   );
 }
