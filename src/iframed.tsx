fe4dbccd60a995f838b8c1865b4027903ac997ef
ah7255703 <ah7255703@gmail.com>
2024-10-23 23:37:23 +0300

    Fix trigger icon color and handle hook states better in IframedWidgetPopover

--- src/iframed.tsx
+++ src/iframed.tsx
@@ -1,17 +1,17 @@
 import { cn } from "@lib/utils/cn";
 import * as PopoverPrimitive from "@radix-ui/react-popover";
 import Iframe from "@uiw/react-iframe";
-import { MessageSquareDot, X } from "lucide-react";
+import { MessageSquareDot, X, XIcon } from "lucide-react";
 import { useState } from "react";
 import styles from "../lib/index.css?inline";
 import { Widget } from "./designs/basic";
 import { cssVars } from "./designs/constants";
 
 export function IframedWidgetPopover() {
-  const [isClicked, setIsClicked] = useState(false);
+  const [isOpen, setIsOpened] = useState(false);
 
   const handleClick = () => {
-    setIsClicked(!isClicked);
+    setIsOpened(!isOpen);
   };
 
   return (
@@ -53,23 +53,21 @@ export function IframedWidgetPopover() {
 
       <PopoverPrimitive.PopoverTrigger
         data-chat-widget
-        className={cn(
-          "bottom-2 right-4 z-[200] fixed p-3 font-inter rounded-full text-white bg-dark transition-transform duration-300 ease-in-out transform active:scale-90",
-          cssVars
-        )}
+        className={`${cssVars} shadow-lg hover:brightness-105 size-fit bottom-2 right-4 transition-all z-[200] fixed font-inter rounded-full text-white bg-primary duration-300 ease-in-out transform active:scale-90`}
         onClick={handleClick}
       >
         <div
           className={cn(
-            "size-6 transition-transform duration-300 ease-in-out",
-            { "transform scale-110": isClicked }
+            "p-3 transition-transform duration-300 relative ease-in-out",
+            { "transform scale-110": isOpen },
           )}
         >
-          {!isClicked ? (
-            <MessageSquareDot className="size-6 transform" />
+          {!isOpen ? (
+            <MessageSquareDot className="size-6" />
           ) : (
-            <X className="size-6" />
+            <XIcon className="size-6" />
           )}
+          <span className="absolute top-0 right-0 size-3 bg-emerald-600 border-2 border-white rounded-full" />
         </div>
       </PopoverPrimitive.PopoverTrigger>
     </PopoverPrimitive.Root>
