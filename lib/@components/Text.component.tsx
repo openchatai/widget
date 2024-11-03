13bade9eeccd5f189f2a24e06429ef17d0cef7e9
ah7255703 <ah7255703@gmail.com>
2024-11-04 00:43:22 +0200

    fix: update Text component styles to enhance link decoration

--- lib/@components/Text.component.tsx
+++ lib/@components/Text.component.tsx
@@ -16,7 +16,7 @@ export function BotTextResponse({
       data-type={type}
       data-id={id}
       remarkPlugins={[remarkGfm]}
-      className="leading-snug font-medium text-sm prose prose-sm prose-slate"
+      className="leading-snug font-medium text-sm prose prose-a:decoration-primary prose-a:underline prose-sm prose-slate"
     >
       {data.message}
     </MemoizedReactMarkdown>
