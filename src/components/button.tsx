5de91dfbff1bd8dd9338c2417e0f2671e32779e9
AliBakerSartawi <alisartawi@live.com>
2025-01-02 17:47:41 +0300

    enhance button styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,19 +5,28 @@ import { cn } from 'src/utils';
 import { Wobble } from './wobble';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  cn(
+    'inline-flex shrink-0 items-center justify-center gap-2',
+    'text-sm font-medium whitespace-nowrap',
+    'ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+    'disabled:pointer-events-none disabled:opacity-50',
+    'shadow-sm active:shadow-none active:scale-95 hover:active:scale-95',
+    // Add border to make transitions between variants smoother... because border takes 1 pixel of space in each direction
+    'border',
+    'transition'
+  ),
   {
     variants: {
       variant: {
-        default: 'bg-primary text-foreground hover:bg-primary/90',
+        default: 'bg-primary text-primary-foreground',
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+          'bg-destructive text-destructive-foreground',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+          'border bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline'
+          'bg-secondary text-secondary-foreground',
+        ghost: 'border-transparent hover:bg-accent hover:text-accent-foreground',
+        link: 'border-transparent text-primary underline-offset-4 hover:underline'
       },
       size: {
         default: 'h-10 px-4 py-2',
