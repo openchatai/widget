ed1f4d0cb6643f8308f6fef7ab74edf664a3734f
AliBakerSartawi <alisartawi@live.com>
2025-01-14 17:25:05 +0300

    add widget sdk

---
+++ core/sdk/index.ts
@@ -0,0 +1,32 @@
+import createClient, { type Middleware } from 'openapi-fetch';
+import type { paths } from './schema';
+import type { components } from './schema';
+
+type Options = {
+  baseUrl: string;
+  onRequest?: Middleware['onRequest'];
+  onResponse?: Middleware['onResponse'];
+  onError?: Middleware['onError'];
+};
+
+const defaultOnError: Middleware['onError'] = (onErrorOptions) => {
+  console.log(onErrorOptions.error);
+};
+
+export const basicClient = (options: Options) => {
+  const client = createClient<paths>({
+    baseUrl: options.baseUrl
+  });
+
+  const middlewares: Middleware = {
+    onRequest: options.onRequest,
+    onResponse: options.onResponse,
+    onError: options.onError || defaultOnError
+  };
+
+  client.use(middlewares);
+  return client;
+};
+
+export type Dto = components['schemas'];
+
