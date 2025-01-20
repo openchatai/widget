203f040bf1579de4376cefe73b2070a41426440d
AliBakerSartawi <alisartawi@live.com>
2025-01-20 15:18:46 +0300

    remove redundant file

--- src/headless/react/hooks.ts
+++
@@ -1,8 +0,0 @@
-export { useMessages } from "./hooks/useMessages";
-export { useSession } from "./hooks/useSession";
-export { useWidget } from "./WidgetProvider";
-export { useConfig } from "./hooks/useConfig";
-export { usePreludeData } from "./hooks/usePreludeData";
-export { type FileWithProgress, useUploadFiles } from "./hooks/useUploadFiles";
-export { useContact } from "./hooks/useContact";
-export { useVote } from "./hooks/useVote";
