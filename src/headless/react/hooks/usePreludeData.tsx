83bde4ba67f3114cb371059427fa69dcaa1bf13e
AliBakerSartawi <alisartawi@live.com>
2025-01-20 14:14:34 +0300

    add comment

--- src/headless/react/hooks/usePreludeData.tsx
+++ src/headless/react/hooks/usePreludeData.tsx
@@ -5,6 +5,7 @@ function usePreludeData() {
   const { widgetCtx } = useWidget();
 
   // TODO remove swr dependency
+  // TODO swr will call this function on page refocus.... get rid of it
   return useSWR([widgetCtx.config.token], widgetCtx.api.widgetPrelude);
 }
 
