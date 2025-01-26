fd8113c4614226555ed8f048d68374dd309ecf92
AliBakerSartawi <alisartawi@live.com>
2025-01-26 14:51:25 +0300

    do not refetch prelude on page focus

--- src/headless/react/hooks/usePreludeData.ts
+++ src/headless/react/hooks/usePreludeData.ts
@@ -5,8 +5,9 @@ function usePreludeData() {
   const { widgetCtx } = useWidget();
 
   // TODO remove swr dependency
-  // TODO swr will call this function on page refocus.... get rid of it
-  return useSWR([widgetCtx.config.token], widgetCtx.api.widgetPrelude);
+  return useSWR([widgetCtx.config.token], widgetCtx.api.widgetPrelude, {
+    revalidateOnFocus: false,
+  });
 }
 
 export { usePreludeData };
