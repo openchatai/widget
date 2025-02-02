f8b8900e3fd04a3a41b4a3c35f563797e7df4fd6
AliBakerSartawi <alisartawi@live.com>
2025-02-02 13:11:05 +0300

    fix isAwaitingBotReply

--- src/headless/react/hooks/useIsAwaitingBotReply.ts
+++ src/headless/react/hooks/useIsAwaitingBotReply.ts
@@ -5,10 +5,14 @@ export function useIsAwaitingBotReply() {
   const { sessionState } = useSessions();
   const { messagesState } = useMessages();
 
+  const isSessionAssignedToAI = sessionState.session?.assignee.kind === "ai";
+  // This check is useful in cases where the user might navigate in and out of a chat, and `isSendingMessage` is reset back to its default value
+  const isLastMessageAUserMessage =
+    messagesState.messages.at(-1)?.type === "FROM_USER";
+
   const isAwaitingBotReply =
-    (sessionState.session?.assignee.kind === "ai" ||
-      sessionState.isCreatingSession) &&
-    messagesState.isSendingMessage;
+    (isSessionAssignedToAI || sessionState.isCreatingSession) &&
+    (messagesState.isSendingMessage || isLastMessageAUserMessage);
 
   return { isAwaitingBotReply };
 }
