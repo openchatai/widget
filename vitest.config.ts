83922bad25097e3417b7881f7c935d4797e591af
AliBakerSartawi <alisartawi@live.com>
2025-02-04 08:34:41 +0300

    remove playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,17 +3,13 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  clearScreen: true,
-  logLevel: "info",
+  plugins: [tsconfigPaths(), react()],
   test: {
-    testTimeout: 10 * 1000,
     typecheck: {
       enabled: true,
     },
     printConsoleTrace: true,
     environment: "jsdom",
     globals: true,
-    setupFiles: ["./vitest/setup.ts"],
   },
-  plugins: [tsconfigPaths(), react()],
 });
