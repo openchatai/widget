b8a0687df4268c5cbb7b0fc24506681c0e464d05
AliBakerSartawi <alisartawi@live.com>
2025-02-09 13:42:04 +0300

    remove react-native-example

--- examples/rn-example/hooks/usePubsub.ts
+++
@@ -1,6 +0,0 @@
-import { useSyncExternalStore } from "react";
-import { PubSub } from "@opencx/widget";
-
-export function usePubsub<S>(pubsub: PubSub<S>): S {
-  return useSyncExternalStore(pubsub.subscribe, pubsub.getState);
-}
