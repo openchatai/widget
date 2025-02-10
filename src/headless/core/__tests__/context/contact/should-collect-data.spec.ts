a03e26e2a663e61a8ff06a02c242d41610489755
AliBakerSartawi <alisartawi@live.com>
2025-02-10 16:24:26 +0300

    test: add tests for contact data collection logic in WidgetCtx

---
+++ src/headless/core/__tests__/context/contact/should-collect-data.spec.ts
@@ -0,0 +1,87 @@
+import "../../api-caller.mock";
+
+import { WidgetCtx } from "../../../context/widget.ctx";
+import { TestUtils } from "../../test-utils";
+
+suite.concurrent("ContactCtx.shouldCollectData", () => {
+  it("Should be always false if not defined in config", async () => {
+    const widgetCtx = new WidgetCtx({ config: { token: "" } });
+    expect(widgetCtx.contactCtx.shouldCollectData()).toBeFalsy();
+    // Wait until auto-create finishes
+    await TestUtils.sleep(100);
+    expect(widgetCtx.contactCtx.shouldCollectData()).toBeFalsy();
+  });
+
+  it("Should be always false if set to false in config", async () => {
+    const widgetCtx = new WidgetCtx({
+      config: { token: "", collectUserData: false },
+    });
+    expect(widgetCtx.contactCtx.shouldCollectData()).toBeFalsy();
+    // Wait until auto-auth finishes
+    await TestUtils.sleep(100);
+    expect(widgetCtx.contactCtx.shouldCollectData()).toBeFalsy();
+  });
+
+  suite("if set to `true` in config", () => {
+    it("Should remain `true` if no user is provided", async () => {
+      const widgetCtx = new WidgetCtx({
+        config: { token: "", collectUserData: true },
+      });
+      expect(widgetCtx.contactCtx.shouldCollectData()).toBeTruthy();
+
+      // Wait until auto-auth finishes
+      await TestUtils.sleep(100);
+      expect(widgetCtx.contactCtx.shouldCollectData()).toBeTruthy();
+    });
+
+    it("Should be initially `false` and remain `false` if user token is provided", async () => {
+      const widgetCtx = new WidgetCtx({
+        config: {
+          token: "",
+          collectUserData: true,
+          user: { token: "some-token" },
+        },
+      });
+      expect(widgetCtx.contactCtx.shouldCollectData()).toBeFalsy();
+
+      // Wait until auto-auth finishes
+      await TestUtils.sleep(100);
+      expect(widgetCtx.contactCtx.shouldCollectData()).toBeFalsy();
+    });
+
+    it("Should become `false` after auto-auth (user data)", async () => {
+      const widgetCtx = new WidgetCtx({
+        config: {
+          token: "",
+          collectUserData: true,
+          user: { data: { email: "test@email.com" } },
+        },
+      });
+      expect(widgetCtx.contactCtx.shouldCollectData()).toBeTruthy();
+
+      // Wait until auto-auth finishes
+      await TestUtils.sleep(100);
+      expect(widgetCtx.contactCtx.shouldCollectData()).toBeFalsy();
+    });
+
+    it("Should become `false` after manual auth (user data)", async () => {
+      const widgetCtx = new WidgetCtx({
+        config: {
+          token: "",
+          collectUserData: true,
+        },
+      });
+      expect(widgetCtx.contactCtx.shouldCollectData()).toBeTruthy();
+
+      // Mimic inputting name and email
+      await widgetCtx.contactCtx.createUnverifiedContact({
+        email: "test@email.com",
+        name: "some-name",
+      });
+
+      expect(widgetCtx.contactCtx.shouldCollectData()).toBeFalsy();
+    });
+  });
+
+  // TODO add tests for auth persistence when `storage` is provided to the widget context
+});
