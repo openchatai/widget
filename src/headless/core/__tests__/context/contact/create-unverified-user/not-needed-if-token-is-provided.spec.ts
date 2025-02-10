deb073f2b7e7ad07d69d8dfea7759a91f91760a1
AliBakerSartawi <alisartawi@live.com>
2025-02-10 12:15:58 +0300

    test: create unverified contact is not called when user token is provided

---
+++ src/headless/core/__tests__/context/contact/create-unverified-user/not-needed-if-token-is-provided.spec.ts
@@ -0,0 +1,24 @@
+import "../../../api-caller.mock";
+
+import { ApiCaller } from "../../../../api/api-caller";
+import { WidgetCtx } from "../../../../context/widget.ctx";
+import { TestUtils } from "../../../test-utils";
+
+suite("", () => {
+  test("", async () => {
+    expect(ApiCaller.prototype.createUnverifiedContact).toBeCalledTimes(0);
+    const widgetCtx = new WidgetCtx({
+      config: { token: "", user: { token: "xyz", externalId: "abc" } },
+    });
+
+    expect(widgetCtx.contactCtx.shouldCollectData()).toBeFalsy();
+    expect(widgetCtx.contactCtx.state.get().contact?.token).toBe("xyz");
+    expect(widgetCtx.contactCtx.state.get().contact?.externalId).toBe("abc");
+
+    await TestUtils.sleep(100);
+
+    expect(ApiCaller.prototype.createUnverifiedContact).toBeCalledTimes(0);
+    expect(widgetCtx.contactCtx.state.get().contact?.token).toBe("xyz");
+    expect(widgetCtx.contactCtx.state.get().contact?.externalId).toBe("abc");
+  });
+});
