c9866724b5cbf82ea05fdc6a4f4451c13e9e768e
AliBakerSartawi <alisartawi@live.com>
2025-02-25 12:54:32 +0300

    fix: refresh sessions only on session creation not on every sent message

--- src/headless/core/context/message.ctx.ts
+++ src/headless/core/context/message.ctx.ts
@@ -121,11 +121,12 @@ export class MessageCtx {
           console.error("Failed to create session");
           return;
         }
+
+        // Refresh sessions list instantly to get the newly created session in the sessions list
+        void this.sessionCtx.refreshSessions();
       }
       const sessionId = this.sessionCtx.sessionState.get().session?.id;
       if (!sessionId) return;
-      // Refresh sessions list instantly to get the newly created session in the sessions list
-      void this.sessionCtx.refreshSessions();
       /* ------------------------------------------------------ */
       /*             Send and wait for bot response             */
       /* ------------------------------------------------------ */
