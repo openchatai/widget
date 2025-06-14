afdba87bdc6deb773eacca88319a180ec9f4336d
AliBakerSartawi <alisartawi@live.com>
2025-06-14 06:51:34 +0300

    untrack `app.dev.tsx`

--- index.dev.tsx
+++ index.dev.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+// create your own app.dev.tsx, refer to app.dev.example.tsx
 import { DevApp } from './app.dev';
 
 const rootElement = document.getElementById('root');
