eac59498dc1b81c57048ec608b02e7e2d481a878
AliBakerSartawi <alisartawi@live.com>
2025-07-07 16:25:22 +0300

    send WidgetConfig.language with contact messages

--- src/headless/core/context/message.ctx.ts
+++ src/headless/core/context/message.ctx.ts
@@ -148,6 +148,7 @@ export class MessageCtx {
             ...(this.config.messageCustomData || {}),
             ...(input.customData || {}),
           },
+          language: this.config.language,
         },
         this.sendMessageAbortController.signal,
       );
